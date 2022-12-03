cd562e68c9e2bb4f98f656a02175aedacc60354e
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-12-03 13:07:08 +0100

    Change to graphOS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   stripeKey: 'pk_live_cwWIdXXGPsJXy0UDC26mL4ai',
-  server: 'https://server.esn.world',
-  useApiPath: true,
+  server: 'https://main--esn.apollographos.net',
+  useApiPath: false,
   version: 'PROD_VERSION',
 };
